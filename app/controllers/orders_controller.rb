d609d2c591f414c0e915083f3e4157913b2205dc
John Devitt <johndevittire@gmail.com>
2014-11-07 19:24:25 +0000

    adding initial tsp code

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,6 +25,10 @@ class OrdersController < ApplicationController
   def delivery_locations
     puts "----------------------------------------------------------------------------------------"
       @orders = Order.where(fulfilled: false)
+
+      salesman = Salesman.new @orders
+      @orders = salesman.solve
+
       @hash = Gmaps4rails.build_markers(@orders) do |order, marker|
         marker.lat order.latitude
         marker.lng order.longitude
@@ -111,4 +115,237 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:address, :town, :county, :phone)
     end
+end
+
+
+
+
+################################################# TSP CODE ################################################################
+
+
+
+
+class Numeric
+  def to_rad
+    self * Math::PI / 180
+  end
+end
+
+class City
+
+  def initialize(order)
+    @order = order
+  end
+
+  def getName()
+    return @order.address
+  end
+
+  def getLatitude()
+    return @order.latitude
+  end
+
+  def getLongitude()
+    return @order.longitude
+  end
+
+  def getOrder
+    return @order
+  end
+
+  def getDistance(city)
+    latitudeDifference = (city.getLatitude - @order.latitude).to_rad;
+    longitudeDifference = (city.getLongitude - @order.longitude).to_rad;
+    a = Math.sin(latitudeDifference / 2) * Math.sin(latitudeDifference / 2) +
+    Math.cos(@order.latitude.to_rad) * Math.cos(city.getLatitude.to_rad) *
+    Math.sin(longitudeDifference/2) * Math.sin(longitudeDifference/2);
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    d = 6371 * c; # Multiply by 6371 to get Kilometers
+    return d
+  end
+end
+
+
+
+class TourManager
+
+  def initialize(cities)
+    @cities = cities
+  end
+
+  def addCity(city)
+    @cities.push city
+  end
+
+  def getCity(index)
+    return @cities.at index
+  end
+
+  def removeCity(city)
+    @cities.delete(city)
+  end
+
+  def getSize()
+    return @cities.size
+  end
+
+end
+
+
+class Tour
+
+  def initialize(cities)
+    @cities = cities
+  end
+
+  def setCity(index, city)
+    @cities[index] = city
+  end
+
+  def addCity(city)
+    @cities.push city
+  end
+
+  def getCity(index)
+    return @cities.at(index)
+  end
+
+  def removeCity(city)
+    @cities.delete(city)
+  end
+
+  def getDistance()
+    total = 0
+
+    for i in 0...getSize - 2
+      total += @cities.at(i).getDistance(@cities.at(i + 1))
+    end
+
+    return total
+  end
+
+  def getSize()
+    return @cities.size
+  end
+
+end
+
+class GreedyAlgorithm
+
+  def generateInitialTour (manager)
+    
+    tour = Tour.new Array.new
+    current = manager.getCity 0
+    tour.addCity current
+    manager.removeCity current
+
+    for i in 0..manager.getSize - 1
+      current = findClosestTown(manager, current)
+      tour.addCity current
+      manager.removeCity current
+    end
+
+    return tour
+  end
+
+  def findClosestTown(manager, city)
+    
+    closestYet = manager.getCity 0
+
+    for i in 0..manager.getSize - 1
+
+      if city.getDistance(manager.getCity(i)) < city.getDistance(closestYet)
+        closestYet = manager.getCity(i)
+      end
+    end
+
+    return closestYet
+  end
+
+  def twoOptSwap(tour, i, j)
+    newTour = Tour.new Array.new
+
+    for x in 0..(i-1)
+      newTour.addCity tour.getCity(x)
+    end
+
+    count = 0
+    for y in i..j
+      city = tour.getCity(j - count)
+      newTour.addCity(city)
+      count = count + 1
+    end
+
+    for z in (j + 1)..(tour.getSize - 1)
+      newTour.addCity(tour.getCity(z))
+    end
+
+    return newTour
+  end
+
+  def twoOptSearch(tour, bestDistance)
+
+    for i in 0..tour.getSize - 1
+      for j in i..tour.getSize - 1
+       testRoute = twoOptSwap tour, i, j
+        if testRoute.getDistance < bestDistance
+          puts testRoute.getDistance
+          puts bestDistance
+          puts "-------------------------------------------------------"
+         tour = testRoute
+         return tour
+        end
+      end
+    end
+
+    return tour
+
+  end
+
+  def twoOptAlgorithm(tour)
+    
+    improved = true
+    while(improved == true)
+
+      bestDistance = tour.getDistance
+      tour = twoOptSearch tour, bestDistance
+      if tour.getDistance == bestDistance
+        puts bestDistance
+        improved = false
+      end
+    end
+
+    return tour
+  end
+
+end
+
+
+
+class Salesman
+
+  def initialize(orders)
+    @orders = orders
+  end
+
+  def solve()
+
+    manager = TourManager.new Array.new
+
+    @orders.each do |order|
+      manager.addCity City.new order
+    end
+
+    algorithm = GreedyAlgorithm.new
+    greedyTour = algorithm.generateInitialTour manager
+    optimizedTour = algorithm.twoOptAlgorithm greedyTour
+
+    result = Array.new
+    for city in 0..optimizedTour.getSize - 1
+      result.push optimizedTour.getCity(city).getOrder
+    end
+
+    return result
+  end
+
 end
---
+++ lib/Runner.rb
@@ -0,0 +1,131 @@
+#!/usr/bin/env ruby
+
+class Numeric
+	def to_rad
+		self * Math::PI / 180
+	end
+end
+
+class City
+
+	def initialize(order)
+		@order = order
+	end
+
+	def getName()
+		return @order.address
+	end
+
+	def getLatitude()
+		return @order.latitude
+	end
+
+	def getLongitude()
+		return @order.longitude
+	end
+
+	def getOrder
+		return @order
+	end
+
+	def getDistance(city)
+		latitudeDifference = (city.getLatitude - @order.latitude).to_rad;
+		longitudeDifference = (city.getLongitude - @order.longitude).to_rad;
+		a = Math.sin(latitudeDifference / 2) * Math.sin(latitudeDifference / 2) +
+		Math.cos(@order.latitude.to_rad) * Math.cos(city.getLatitude.to_rad) *
+		Math.sin(longitudeDifference/2) * Math.sin(longitudeDifference/2);
+		c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		d = 6371 * c; # Multiply by 6371 to get Kilometers
+		return d
+	end
+end
+
+
+
+class TourManager
+
+	def initialize(cities)
+		@cities = cities
+	end
+
+	def addCity(city)
+		@cities.push city
+	end
+
+	def getCity(index)
+		return @cities.at index
+	end
+
+	def removeCity(city)
+		@cities.delete(city)
+	end
+
+	def getSize()
+		return @cities.size
+	end
+
+end
+
+class GreedyAlgorithm
+
+	def generateInitialTour (manager, depot)
+		
+		tour = Array.new
+		current = findClosestTown(manager, depot)
+		tour.push current
+		manager.removeCity(current)
+
+
+		for i in 0..manager.getSize - 1
+			current = findClosestTown(manager, current)
+			tour.push current
+			manager.removeCity current
+		end
+
+		return tour
+	end
+
+	def findClosestTown(manager, city)
+		
+		closestYet = manager.getCity 0
+
+		for i in 0..manager.getSize - 1
+
+			if city.getDistance(manager.getCity(i)) < city.getDistance(closestYet)
+				closestYet = manager.getCity(i)
+			end
+		end
+
+		return closestYet
+	end
+
+end
+
+
+
+class Salesman
+
+	def initialize(orders)
+		@orders = orders
+	end
+
+	def solve()
+
+		manager = TourManager.new
+
+		@orders.each do |order|
+			manager.addCity City.new order
+		end
+
+		depot = City.new 53.3880126, -6.3843862, "depot"
+
+		algorithm = GreedyAlgorithm.new
+		greedyTour = algorithm.generateInitialTour manager, depot
+
+		result = Array.new
+		greedyTour.each do |city|
+			result.push city.getOrder
+		end
+	end
+
+end
