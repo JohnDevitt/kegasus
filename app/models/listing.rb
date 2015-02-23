d9d36a4d7611c6fdf52f808b6343c94c6f1aab7f
John Devitt <johndevittire@gmail.com>
2015-02-23 18:30:55 +0000

    gmaps fix

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,8 +6,7 @@ class Listing < ActiveRecord::Base
 	  else
 	  has_attached_file :image, :styles => { :medium => "200x200", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
 	  							:storage => :dropbox,
-    							:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    							:path => ":style/:id_:filename"
+    							:dropbox_credentials => Rails.root.join("config/dropbox.yml")
    		end
 	  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
